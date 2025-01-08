1f37900cde96b95621ba478a1506074ed2f1386d
Babakoki <s2422055@stu.musahino-u.ac.jp>
2025-01-08 11:08:02 +0900

    DBの作成

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+import requests
+import sqlite3
+import os
+import json
+import time
+from bs4 import BeautifulSoup
+import pandas as pd
 import flet as ft
+
+db_name = "final_task.db"
+
+# final_task.dbという名前のデータベースファイルを作成する
+if not os.path.exists(db_name):
+    con = sqlite3.connect(db_name)
+    cur = con.cursor()
+    sql = 'CREATE TABLE IF NOT EXISTS treasure (prefectures INTEGER PRIMARY KEY, Number_of_pieces int)'
+    sql2 = 'CREATE TABLE IF NOT EXISTS evaluation (prefectures INTEGER PRIMARY KEY, price int, evaluation int)'
+    cur.execute(sql)
+    cur.execute(sql2)
+    con.close()
 
 
 def main(page: ft.Page):
